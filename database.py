711d52030c2e74c39e6edc92992413b229d1444e
Léo Da Rocha <leo.da.rocha2002@gmail.com>
2024-06-02 19:56:10 +0200

    create & update by permissions

--- database.py
+++ database.py
@@ -116,3 +116,134 @@ def get_all_events():
     events = db.query(Event).all()
     db.close()
     return events
+
+
+def create_collaborator(employee_number, full_name, email, department, hashed_password, role_id):
+    db = SessionLocal()
+    new_user = User(
+        employee_number=employee_number,
+        full_name=full_name,
+        email=email,
+        department=department,
+        hashed_password=hashed_password,
+        role_id=role_id
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    db.close()
+    return new_user
+
+
+def update_collaborator(user_id, full_name=None, email=None, department=None, role_id=None):
+    db = SessionLocal()
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        db.close()
+        return None
+    if full_name:
+        user.full_name = full_name
+    if email:
+        user.email = email
+    if department:
+        user.department = department
+    if role_id:
+        user.role_id = role_id
+    db.commit()
+    db.refresh(user)
+    db.close()
+    return user
+
+
+def create_contract(client_id, commercial_contact, total_amount, amount_due, creation_date, status):
+    db = SessionLocal()
+    new_contract = Contract(
+        client_id=client_id,
+        commercial_contact=commercial_contact,
+        total_amount=total_amount,
+        amount_due=amount_due,
+        creation_date=creation_date,
+        status=status
+    )
+    db.add(new_contract)
+    db.commit()
+    db.refresh(new_contract)
+    db.close()
+    return new_contract
+
+
+def update_contract(contract_id, client_id=None, commercial_contact=None,
+                    total_amount=None, amount_due=None, creation_date=None, status=None):
+    db = SessionLocal()
+    contract = db.query(Contract).filter(Contract.id == contract_id).first()
+    if not contract:
+        db.close()
+        return None
+    if client_id:
+        contract.client_id = client_id
+    if commercial_contact:
+        contract.commercial_contact = commercial_contact
+    if total_amount:
+        contract.total_amount = total_amount
+    if amount_due:
+        contract.amount_due = amount_due
+    if creation_date:
+        contract.creation_date = creation_date
+    if status:
+        contract.status = status
+    db.commit()
+    db.refresh(contract)
+    db.close()
+    return contract
+
+
+def create_event(contract_id, client_name, client_contact, event_date_start,
+                 event_date_end, support_contact, location, attendees, notes):
+    db = SessionLocal()
+    new_event = Event(
+        contract_id=contract_id,
+        client_name=client_name,
+        client_contact=client_contact,
+        event_date_start=event_date_start,
+        event_date_end=event_date_end,
+        support_contact=support_contact,
+        location=location,
+        attendees=attendees,
+        notes=notes
+    )
+    db.add(new_event)
+    db.commit()
+    db.refresh(new_event)
+    db.close()
+    return new_event
+
+
+def update_event(event_id, contract_id=None, client_name=None, client_contact=None, event_date_start=None,
+                 event_date_end=None, support_contact=None, location=None, attendees=None, notes=None):
+    db = SessionLocal()
+    event = db.query(Event).filter(Event.id == event_id).first()
+    if not event:
+        db.close()
+        return None
+    if contract_id:
+        event.contract_id = contract_id
+    if client_name:
+        event.client_name = client_name
+    if client_contact:
+        event.client_contact = client_contact
+    if event_date_start:
+        event.event_date_start = event_date_start
+    if event_date_end:
+        event.event_date_end = event_date_end
+    if support_contact:
+        event.support_contact = support_contact
+    if location:
+        event.location = location
+    if attendees:
+        event.attendees = attendees
+    if notes:
+        event.notes = notes
+    db.commit()
+    db.refresh(event)
+    db.close()
+    return event
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import configparser
 import argparse
-from database import SessionLocal, Client, Contract, Event, User, get_all_clients, get_all_contracts, get_all_events
-from auth import decode_jwt_token, has_permission
+from datetime import date
+from database import SessionLocal, User, Role, Client, create_contract, update_contract, create_event, update_event, \
+    update_collaborator
+from auth import decode_jwt_token, has_permission, create_user
 
 parser = argparse.ArgumentParser(description="Epicevents Script")
 parser.add_argument('--token', type=str, help="JWT token to use for authentication")
@@ -11,7 +13,6 @@ args = parser.parse_args()
 if args.token:
     token = args.token
 else:
-
     config = configparser.ConfigParser()
     config.read('config.ini')
     token = config['DEFAULT'].get('JWT_TOKEN')
@@ -30,36 +31,106 @@ try:
         raise ValueError("User not found")
     print(f"Authenticated user: {user.full_name}")
 
-    if has_permission(user, "view_clients"):
-        print("User has permission to view clients.")
-        clients = get_all_clients()
-        print("Clients:")
-        for client in clients:
-            print(f"{client.full_name}, {client.email}, {client.company_name}")
-    else:
-        print("User does not have permission to view clients.")
+    if has_permission(user, "create_user"):
+        new_collaborator = create_user(
+            employee_number="44444",
+            full_name="New Collaborator",
+            email="newcollab@example.com",
+            department="Sales",
+            password="password_example",
+            role_name="commercial"
+        )
+        print(f"Created Collaborator: ID: {new_collaborator.id}, Employee Number: {new_collaborator.employee_number}, "
+              f"Full Name: {new_collaborator.full_name}, Email: {new_collaborator.email}, "
+              f"Department: {new_collaborator.department}, Role: {new_collaborator.role.name}")
 
-    if has_permission(user, "view_contracts"):
-        print("User has permission to view contracts.")
-        contracts = get_all_contracts()
-        print("Contracts:")
-        for contract in contracts:
-            print(f"Contract {contract.id} for client {contract.client_id}, "
-                  f"total amount: {contract.total_amount}, "
-                  f"status: {contract.status}")
-    else:
-        print("User does not have permission to view contracts.")
+    if has_permission(user, "update_user"):
+        role = db.query(Role).filter(Role.name == "support").first()
+        if not role:
+            raise ValueError("Role not found")
 
-    if has_permission(user, "view_events"):
-        print("User has permission to view events.")
-        events = get_all_events()
-        print("Events:")
-        for event in events:
-            print(f"Event {event.id} for contract {event.contract_id}, "
-                  f"location: {event.location}, "
-                  f"date: {event.event_date_start} to {event.event_date_end}")
-    else:
-        print("User does not have permission to view events.")
+        updated_collaborator = update_collaborator(
+            user_id=new_collaborator.id,
+            full_name="Updated Collaborator",
+            email="updatedcollab@example.com",
+            department="Marketing",
+            role_id=role.id
+        )
+
+        updated_collaborator = db.get(User, updated_collaborator.id)
+        print(f"Updated Collaborator: ID: {updated_collaborator.id}, "
+              f"Employee Number: {updated_collaborator.employee_number}, Full Name: {updated_collaborator.full_name}, "
+              f"Email: {updated_collaborator.email}, Department: {updated_collaborator.department}, "
+              f"Role: {updated_collaborator.role.name}")
+
+    if has_permission(user, "create_contract"):
+        new_client = Client(
+            full_name="Kevin Casey",
+            email="kevin@startup.io",
+            phone_nb="+678 123 456 78",
+            company_name="Cool Startup LLC",
+            first_contact_date=date(2021, 4, 18),
+            last_contact_date=date(2023, 3, 29),
+            commercial_contact="Bill Boquet"
+        )
+        db.add(new_client)
+        db.commit()
+        db.refresh(new_client)
+        print(f"Created Client: ID: {new_client.id}, Full Name: {new_client.full_name}, Email: {new_client.email}, "
+              f"Phone: {new_client.phone_nb}, Company Name: {new_client.company_name}, "
+              f"First Contact Date: {new_client.first_contact_date}, Last Contact Date: {new_client.last_contact_date},"
+              f" Commercial Contact: {new_client.commercial_contact}")
+
+        new_contract = create_contract(
+            client_id=new_client.id,
+            commercial_contact=new_client.commercial_contact,
+            total_amount=20000.0,
+            amount_due=10000.0,
+            creation_date=date.today(),
+            status="Pending"
+        )
+        db.add(new_contract)
+        db.commit()
+        db.refresh(new_contract)
+        print(f"Created Contract: ID: {new_contract.id}, Client ID: {new_contract.client_id}, "
+              f"Commercial Contact: {new_contract.commercial_contact}, Total Amount: {new_contract.total_amount}, "
+              f"Amount Due: {new_contract.amount_due}, Creation Date: {new_contract.creation_date}, "
+              f"Status: {new_contract.status}")
+
+    if has_permission(user, "update_contract"):
+        updated_contract = update_contract(
+            contract_id=new_contract.id,
+            status="Signed"
+        )
+        print(f"Updated Contract: ID: {updated_contract.id}, Client ID: {updated_contract.client_id}, "
+              f"Commercial Contact: {updated_contract.commercial_contact}, "
+              f"Total Amount: {updated_contract.total_amount}, Amount Due: {updated_contract.amount_due}, "
+              f"Creation Date: {updated_contract.creation_date}, Status: {updated_contract.status}")
+
+    if has_permission(user, "create_event"):
+        new_event = create_event(
+            contract_id=new_contract.id,
+            client_name=new_client.full_name,
+            client_contact=new_client.email,
+            event_date_start=date(2023, 6, 4),
+            event_date_end=date(2023, 6, 5),
+            support_contact="Support Contact",
+            location="Event Location",
+            attendees=100,
+            notes="Event Notes"
+        )
+        db.add(new_event)
+        db.commit()
+        db.refresh(new_event)
+        print(f"Created Event: ID: {new_event.id}, Contract ID: {new_event.contract_id}, Client Name: {new_event.client_name}, Client Contact: {new_event.client_contact}, Event Start Date: {new_event.event_date_start}, Event End Date: {new_event.event_date_end}, Support Contact: {new_event.support_contact}, Location: {new_event.location}, Attendees: {new_event.attendees}, Notes: {new_event.notes}")
+
+    if has_permission(user, "update_event"):
+        updated_event = update_event(
+            event_id=new_event.id,
+            notes="Updated Event Notes"
+        )
+        print(f"Updated Event: ID: {updated_event.id}, Contract ID: {updated_event.contract_id}, Client Name: {updated_event.client_name}, Client Contact: {updated_event.client_contact}, Event Start Date: {updated_event.event_date_start}, Event End Date: {updated_event.event_date_end}, Support Contact: {updated_event.support_contact}, Location: {updated_event.location}, Attendees: {updated_event.attendees}, Notes: {updated_event.notes}")
+
 except ValueError as e:
     print(e)
 finally:
